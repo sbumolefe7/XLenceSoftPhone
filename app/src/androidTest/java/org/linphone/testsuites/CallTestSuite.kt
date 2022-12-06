d87c5ca616f02bd6e721edf09397baf85bcfc5ae
Quentin Monnier <quentin.monnier38@gmail.com>
2022-12-06 11:30:10 -0500

    update gitlab-ci and screport submodule

--- app/src/androidTest/java/org/linphone/testsuites/CallTestSuite.kt
+++ app/src/androidTest/java/org/linphone/testsuites/CallTestSuite.kt
@@ -2,10 +2,12 @@ package org.linphone.testsuites
 
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
+import org.linphone.call.IncomingCallUITests
 import org.linphone.call.OutgoingCallUITests
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    OutgoingCallUITests::class
+    OutgoingCallUITests::class,
+    IncomingCallUITests::class
 )
 class CallTestSuite
